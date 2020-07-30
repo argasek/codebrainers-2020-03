6ccdd5e93efd37856b29b2146a855f1c5887f165
Szymon Tylmanowski <smile.tylmanowski@gmail.com>
2020-07-30 19:27:04 +0200

    Use isNaN in order to determine type of input variable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-console.log('Hello world from developers\' console!');
-let userName = prompt("Hello world! What's your name?");
-alert(`Nice to meet you, ${userName}!`);
+let age = parseInt(prompt("What's your age?"));
+console.log(age);
+
+if (!isNaN(age){
+    console.log('Ok');
+} else {
+    console.log('Not Ok');
+}
+// if (age >= 18) {
+//     alert(`Nice to meet you, you have ${age}!`);
+// } else {
+//     alert('You are not have 18 years old');
+// }
