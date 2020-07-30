b38aea1c424e73a5ef62e774ff1250344e96301a
Jakub Argasiński <jakub@argasinski.eu>
2020-07-30 21:02:59 +0200

    Homework! :-)

--- src/index.js
+++ src/index.js
@@ -17,3 +17,19 @@ for (let i = 0; i < liczbaPytan; i++) {
     }
 }
 console.log('Liczba poprawnych odpowiedzi ' + poprawneOdpowiedzi);
+
+/*
+Zadanie 1. Metodą kopiuj-wklej odnajdź w internetach algorytm
+generowania liczby -całkowitej- z zakresu od 1..10.
+
+Zadanie 2. Po wykonaniu kopiuj wklej przenalizuj ten algorytm
+(ten = tj. algorytm losujący) i zastanów się, w jaki sposób on
+działa. W dokumentacji na stronie MDN sprawdź jak działa funkcja
+Math.random().
+
+Zadanie 3. Wypisz na ekran czy użytkownik zaliczył test (musi mieć
+co najmniej 6 poprawnych odpowiedzi)
+
+Zadanie 4*. Wypisz ocenę na podstawie udzielonych odpowiedzi, zakładając
+że 60% = 2 (dopuszczająca) a 100% oznacza szkolną "szóstkę".
+ */
