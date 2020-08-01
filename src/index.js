bf64f5d86bc13e7798f90841dcc9d7fc0655af52
Anna <adurak93@gmail.com>
2020-08-01 02:17:43 -0700

    Ternary operator

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 const arr = [1, 5, 7, false, 'stringg', true, 545];
-function mapper(item, index, a){
-    return a[a.length - index - 1];
-}
-// const newArr = function(item, index, arra){
+
+let newArr = arr.map((item) => typeof item === "number" ? item : 0);
 
 
-//     console.log(item, index*-1, arra)
+console.log(newArr);
+
+// function mapper(item, index, a){
+//     return a[a.length - index - 1];
 // }
+// // const newArr = function(item, index, arra){
 
-// arr.map(newArr)
 
-const arr2 = arr.map(mapper);
+// //     console.log(item, index*-1, arra)
+// // }
 
-//for (let i = arr.length-1, j = 0; i>=0; i--, j++){
- //  arr2[j] = arr[i];
-   // console.log(arr[i]);
-// }
+// // arr.map(newArr)
 
-console.log(arr2)
+// const arr2 = arr.map(mapper);
+
+// //for (let i = arr.length-1, j = 0; i>=0; i--, j++){
+//  //  arr2[j] = arr[i];
+//    // console.log(arr[i]);
+// // }
+
+// console.log(arr2)
 
