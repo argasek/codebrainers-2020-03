53c847496048e4195a05dcf55bab226394dfd544
Jakub Argasiński <jakub@argasinski.eu>
2020-08-01 16:58:18 +0200

    Demonstrate sorting of arrays.

--- src/index.js
+++ src/index.js
@@ -1,70 +1,41 @@
-/*
-algorytm losuje tak długo 6 liczb dopóki nie wylosuje zadanego zestawu
-losujemy liczby z zakresu 1-49
-wylosowanie pierwszej liczby powoduje zawężenie zakresu 1-48 itd..
-*/
+const expectedNumbers = [ 12, 11,  7, 17, 39, 40 ];
+const actualNumbers   = [ 11, 12, 17,  7, 40, 39 ];
 
-const expectedNumbers = [ 7, 11, 12, 17, 39, 40 ];
+/**
+ * @param {array} a
+ * @param {array} b
+ */
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function searchNumber(x) {
-  for (let i = 0; i < expectedNumbers.length; i++) {
-    if (x === expectedNumbers[i]) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function drawNumber(possibleNumbers) {
-  let index = getRandomInt(0, possibleNumbers.length - 1);
-  let x = possibleNumbers[index];
-  possibleNumbers.splice(index, 1);
-  return x;
-}
-
-function compareDrawnNumbers(numbers) {
-  let numberOfMatches = 0;
-  for (let i = 0; i < expectedNumbers.length; i++) {
-    for (let j = 0; j < numbers.length; j++) {
-      if (expectedNumbers[i] === numbers[j]) {
-        numberOfMatches++;
-      }
-    }
-  }
-  return numberOfMatches === expectedNumbers.length;
-}
-
-let drawnNumber;
-let drawnNumbers;
-let number = 0;
-let possibleNumbers;
-
-let numberOfIterations = 0;
-
-do {
-  possibleNumbers = [];
-
-  for (let i = 1; i < 49 + 1; i++) {
-    possibleNumbers.push(i);
+const comparator = (x, y) => {
+  if (x < y) {
+    return -1;
   }
 
-  drawnNumbers = [];
-
-  for (let i = 0; i < 6; i++) {
-    drawnNumber = drawNumber(possibleNumbers);
-    drawnNumbers.push(drawnNumber);
+  if (x > y) {
+    return 1;
   }
 
-  // console.log(drawnNumbers);
-  // console.log(possibleNumbers);
-  numberOfIterations++;
+  return 0;
+};
 
-} while (!compareDrawnNumbers(drawnNumbers) && numberOfIterations < 1000000);
+/**
+ *
+ * @param {array} a
+ * @param {array} b
+ */
+const compareArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return;
+  }
 
-console.log(numberOfIterations);
+  a.sort(comparator);
+  b.sort(comparator);
+
+  const result = a.map((item, index) => item === b[index])
+    .filter((item) => item === false);
+
+  return result.length === 0;
+};
+
+const result = compareArrays(expectedNumbers, actualNumbers);
+console.log(result);
