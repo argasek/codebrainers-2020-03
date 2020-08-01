3e446d85b591dc46f151f59aa36ef897d5ca1c01
Marharyta Sirachenko <sirachenko944@gmail.com>
2020-08-01 10:13:08 +0200

    homework

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const liczbaPytan = 5;
+/*const liczbaPytan = 5;
 
 function getRandomInteger() {
     return 5;
@@ -18,7 +18,8 @@ for (let i = 0; i < liczbaPytan; i++) {
 }
 console.log('Liczba poprawnych odpowiedzi ' + poprawneOdpowiedzi);
 
-/*
+HOMEWORK!!
+
 Zadanie 1. Metodą kopiuj-wklej odnajdź w internetach algorytm
 generowania liczby -całkowitej- z zakresu od 1..10.
 
@@ -33,3 +34,48 @@ co najmniej 6 poprawnych odpowiedzi)
 Zadanie 4*. Wypisz ocenę na podstawie udzielonych odpowiedzi, zakładając
 że 60% = 2 (dopuszczająca) a 100% oznacza szkolną "szóstkę".
  */
+
+
+ /*Math.random() zwraca Liczbę zmiennoprzecinkową, pseudolosowa z przedziału od 0(włącznie) do 1 (wyłącznie).*/
+
+
+
+
+/*const numberOfQuestions = 10;
+let correctAnswer = 0;
+for (let i = 0; i < numberOfQuestions; i++) {
+    let computerAnswer = Math.floor(Math.random() * 10) + 1; // 1 - od ktorej liczby zakres; 10 - do której liczby zakres; Math.floor() zaokrągla liczby w dol< Math.ceil() do góry
+    console.log(computerAnswer);
+    let userAnswer = prompt('Enter a number from 1 to 10');
+    userAnswer = parseInt(userAnswer);
+    if (userAnswer === computerAnswer) {
+        correctAnswer++;
+    }
+  }
+
+if (correctAnswer >= 6) {
+    console.log('You passed the test!!You have ' + correctAnswer + ' correct answers');
+} else {
+    console.log('You didn\'t pass =( Try again!');
+}
+
+function gradeCalc(correctAnswer) { // oblicza %
+    return (correctAnswer / numberOfQuestions) * 100;   
+}
+let a = gradeCalc(correctAnswer); //przechowuje %
+console.log(a + ' %');
+
+if (a < 60) {
+    console.log('Your grade is 1');
+} else if (a >= 60 && a < 100) {
+    console.log('Your grade is 2');
+} else if (a === 100) {
+    console.log('Your grade is 6!!Congratulations!!!=)');
+}
+
+// 6 = 100%
+// 2 = 60%
+//poprawna odpowiedz/6 * 100*/
+
+
+
