cef2d5a0e9299ebcda7bc9bb92a5979ff00fe6bf
Jakub Argasiński <jakub@argasinski.eu>
2020-08-01 16:11:46 +0200

    Fix the algorithm by taking care of possibleNumbers array being accidentally reduced in size with each outer loop iteration.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ losujemy liczby z zakresu 1-49
 wylosowanie pierwszej liczby powoduje zawężenie zakresu 1-48 itd..
 */
 
-const expectedNumbers = [7, 11, 12, 17, 39, 40];
+const expectedNumbers = [ 7, 11, 12, 17, 39, 40 ];
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -13,33 +13,26 @@ function getRandomInt(min, max) {
 }
 
 function searchNumber(x) {
-  for (let i=0; i < expectedNumbers.length; i++) {
-    if(x === expectedNumbers[i]) {
+  for (let i = 0; i < expectedNumbers.length; i++) {
+    if (x === expectedNumbers[i]) {
       return true;
     }
   }
   return false;
 }
 
-let possibleNumbers = [];
-for (let i =1; i < 49 + 1; i++) {
-    possibleNumbers.push(i);
-}
-
 function drawNumber(possibleNumbers) {
-    let index = getRandomInt(0, possibleNumbers.length - 1);
-    let x = possibleNumbers[index];
-    possibleNumbers.splice(index, 1);
-    return x;
+  let index = getRandomInt(0, possibleNumbers.length - 1);
+  let x = possibleNumbers[index];
+  possibleNumbers.splice(index, 1);
+  return x;
 }
-
-let drawnNumber;
 
 function compareDrawnNumbers(numbers) {
-  let numberOfMatches = 0
-  for(let i=0; i<expectedNumbers.length; i++){
-    for(let j =0; j < numbers.length; j++){
-      if(expectedNumbers[i] === numbers[j]){
+  let numberOfMatches = 0;
+  for (let i = 0; i < expectedNumbers.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (expectedNumbers[i] === numbers[j]) {
         numberOfMatches++;
       }
     }
@@ -47,15 +40,31 @@ function compareDrawnNumbers(numbers) {
   return numberOfMatches === expectedNumbers.length;
 }
 
+let drawnNumber;
 let drawnNumbers;
+let number = 0;
+let possibleNumbers;
 
-let number = 0
+let numberOfIterations = 0;
 
 do {
+  possibleNumbers = [];
+
+  for (let i = 1; i < 49 + 1; i++) {
+    possibleNumbers.push(i);
+  }
+
   drawnNumbers = [];
-  for (let i=0; i < 6; i++){
+
+  for (let i = 0; i < 6; i++) {
     drawnNumber = drawNumber(possibleNumbers);
     drawnNumbers.push(drawnNumber);
   }
-  console.log(drawnNumbers);
-} while(!compareDrawnNumbers(drawnNumbers))
+
+  // console.log(drawnNumbers);
+  // console.log(possibleNumbers);
+  numberOfIterations++;
+
+} while (!compareDrawnNumbers(drawnNumbers) && numberOfIterations < 1000000);
+
+console.log(numberOfIterations);
