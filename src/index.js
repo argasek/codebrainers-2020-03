7abfd9b9845f4ae4205a14b9df8487353eff1fca
Kamil MArtynek <kmartynek@gmail.com>
2020-08-01 10:45:14 +0200

    Zadanie nr2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const arr = [1, 5, 7, false, 'stringg', true, 545];
-
+function mapper(item, index, a){
+    return a[a.length - index - 1];
+}
 // const newArr = function(item, index, arra){
 
 
@@ -8,11 +10,12 @@ const arr = [1, 5, 7, false, 'stringg', true, 545];
 
 // arr.map(newArr)
 
-const arr2 = [];
+const arr2 = arr.map(mapper);
 
-for (let i = arr.length-1, j = 0; i>=0; i--, j++){
-    arr2[j] = arr[i];
-    console.log(arr[i]);
-}
+//for (let i = arr.length-1, j = 0; i>=0; i--, j++){
+ //  arr2[j] = arr[i];
+   // console.log(arr[i]);
+// }
 
 console.log(arr2)
+
