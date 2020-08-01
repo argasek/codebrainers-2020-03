730746dd82db3d1df54d3ac136c2c51e4e4b1743
Anna <adurak93@gmail.com>
2020-08-01 01:12:55 -0700

    Homework-01

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const liczbaPytan = 5;
+const liczbaPytan = 10;
 
 function getRandomInteger() {
-    return 5;
+    return Math.floor(Math.random()*10 +1);
 }
 
 let poprawneOdpowiedzi = 0;
+let czyZaliczony, ocena;
+
+alert('Witaj na sprawdzianie z tabliczki mnożenia. Przed Tobą 10 pytań. Wciśnij OK aby zacząć. ');
 
 for (let i = 0; i < liczbaPytan; i++) {
     let a = getRandomInteger();
@@ -16,7 +19,39 @@ for (let i = 0; i < liczbaPytan; i++) {
         poprawneOdpowiedzi++;
     }
 }
-console.log('Liczba poprawnych odpowiedzi ' + poprawneOdpowiedzi);
+
+function obliczOcenę() {
+    if (poprawneOdpowiedzi < liczbaPytan * 0.75) {
+        ocena = 'Twoja ocena to 2 (dopuszczający).';
+    } 
+    else {
+        if (poprawneOdpowiedzi < liczbaPytan * 0.8) {
+            ocena = 'Twoja ocena to 3 (dostateczny).';
+        } else {
+            if (poprawneOdpowiedzi < liczbaPytan * 0.9) {
+            ocena = 'Twoja ocena to 4 (dobry).';
+            } else { 
+                if (poprawneOdpowiedzi < liczbaPytan) {
+                ocena = 'Twoja ocena to 5 (bardzo dobry).';
+                } else {
+                ocena = 'Twoja ocena to 6 (celujący).';
+                }
+            }
+        }
+    }
+    return ocena;
+}
+
+if (poprawneOdpowiedzi >= liczbaPytan * 0.6) {
+    czyZaliczony = 'Gratulacje! Zaliczyłeś sprawdzian. ';
+    obliczOcenę();
+} else {
+    czyZaliczony = 'Niestety nie zaliczyłeś zprawdzianu. ';
+    ocena = 'Twoja ocena to: 1 (niedostateczny).';
+};
+
+alert('Liczba poprawnych odpowiedzi: ' + poprawneOdpowiedzi + '\nLiczba błędnych odpowiedzi: ' + (10 - poprawneOdpowiedzi)+ '\n' + czyZaliczony + '\n' + ocena);
+
 
 /*
 Zadanie 1. Metodą kopiuj-wklej odnajdź w internetach algorytm
