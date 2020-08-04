ec4231f6881bcd94579bf027020ee0ffd164ea32
Jakub Argasiński <jakub@argasinski.eu>
2020-08-04 20:30:15 +0200

    Introduction to map/reduce pattern - continued.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 const timeTable = [
-  ['9:00-17:00'], // 8
-  ['11:30-13:30', '14:00-16:00'], // 2 + 2
-  ['8:00-16:00'], // 8
-  ['11:00-23:50'], // 12h: 50
-  ['03:00-23:10'], // 20 h : 10
-  ['06:00-11:00'], // 5 
+  [ '9:00-17:00', '07:15-07:20' ], // 8h 5m
+  [ '11:30-13:30', '14:00-16:00' ], // 2 + 2
+  [ '8:00-16:00' ], // 8
+  [ '11:00-23:50' ], // 12h: 50
+  [ '03:00-23:10' ], // 20 h : 10
+  [ '06:00-11:00' ], // 5
 ];
 
 // 40h / tygodniowo?
 
-const convertTimeRangeToMinutes = timeRange => timeRange.split('-').map(hourWithMinutesAsString => {
-  const hourWithMinutes = hourWithMinutesAsString.split(':').map(hourMinutePart => {
-    return parseInt(hourMinutePart);
-  }).reduce((accumulator, currentValue) => {
-    return accumulator * 60 + currentValue;
-  });
-  return hourWithMinutes;
-}).reduce((accumulator, currentValue) => {
-  console.log(accumulator, currentValue);
-  return currentValue-accumulator;
-} );
+const convertTimeRangeToMinutes = timeRange => timeRange
+  .split('-')
+  .map(hourWithMinutesAsString => hourWithMinutesAsString
+    .split(':')
+    .map(hourMinutePart => +hourMinutePart)
+    .reduce((hours, minutes) => hours * 60 + minutes)
+  )
+  .reduce((startTimestampAsMinutes, endTimestampAsMinutes) => endTimestampAsMinutes - startTimestampAsMinutes);
 
 console.log(timeTable[3][0]);
 console.log(convertTimeRangeToMinutes(timeTable[3][0]));
