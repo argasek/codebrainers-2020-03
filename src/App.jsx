c5d51423d9d1dd339393cc72bb853326757364c3
Anna <adurak93@gmail.com>
2020-08-09 05:41:32 -0700

    Extract and present data from student object.

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import './App.css';
 import codebrainersStudents from './models/Students';
 
 function App() {
-  const arr = codebrainersStudents.map(function(student) {
-    return student.fullName
+  const arr = codebrainersStudents.map(function (student, index) {
+    return <li key={index}>
+      {student.fullName}
+      {' '}
+      {student.beers}
+      {' '}
+      {student.frequency}
+    </li>;
   });
   console.log(codebrainersStudents);
   return <ul>
-   {arr}
+    {arr}
   </ul>;
 
 }
