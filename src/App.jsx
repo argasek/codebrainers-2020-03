2122fb28d36862f3a6c39a1cf904c1ac0d961428
Anna <adurak93@gmail.com>
2020-08-12 18:32:17 +0200

    Give restrictions about numbers and operational signs to the input field.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
 
-function notDivideByZero(signInput, zeroInput){
-  if((signInput === '/') && (parseInt(zeroInput) === 0)){
+
+function onlyNumber(field) {
+    if (isNaN(parseFloat(field))) {
+      return alert('Tutaj trzeba wpisać liczbę!');
+    }
+}
+
+function onlySign(field) {
+  const signs = ['+', '-', '*', '/', ''];
+  let check = false;
+  for(let i=0; i < signs.length; i++){
+    if(field === signs[i]){
+      check = true;
+      return check;
+    }
+    // return field === signs[i] ? check=true : check
+  }
+  return check ? signs : alert('Tutaj wpisz jeden ze znaków: +, -, * lub /.');
+}
+
+function notDivideByZero(signInput, zeroInput) {
+  if ((signInput === '/') && (parseInt(zeroInput) === 0)) {
     return alert('Nie dziel przez zero Ty... brzydki człowieku.');
   }
 }
@@ -24,13 +44,16 @@ class App extends React.Component {
   }
 
   changeFirst = (event) => {
+    onlyNumber(event.target.value);
     this.setState({firstValue: event.target.value});
   }
   changeSign = (event) => {
+    onlySign(event.target.value);
     notDivideByZero(event.target.value, this.state.secondValue);
     this.setState({inputSign: event.target.value});
   }
   changeSecond = (event) => {
+    onlyNumber(event.target.value);
     notDivideByZero(this.state.inputSign, event.target.value);
     this.setState({secondValue: event.target.value});
   }
