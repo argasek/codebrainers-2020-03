0f40bff6321342b35bcb261332739c3e2430e074
Anna <adurak93@gmail.com>
2020-08-18 17:14:57 +0200

    Refactoring the code included show the messages in invalid situations. Use reactstrap.

--- src/components/inputField.js
+++ src/components/inputField.js
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Input, FormFeedback, Label, FormGroup } from 'reactstrap';
 
-const InputField  = ({field, name, type, onChange, placeholder, errorMessage = ''}) => {
+const InputField  = ({field, name, type, onChange, placeholder, errorMessage = '', onBlur}) => {
   return (
     <td>
       <FormGroup>
-      <Label htmlFor={name}></Label>
+      <Label htmlFor={name}>{name}: <br/></Label>
       <Input
         value={field}
+        // errorMessage = {errorMessage}
         id={name}
         type={type}
         onChange={onChange}
         placeholder={placeholder}
         invalid={errorMessage !== ''}
+        onBlur = {onBlur}
       />
       <FormFeedback>{errorMessage}</FormFeedback>
       </FormGroup>
