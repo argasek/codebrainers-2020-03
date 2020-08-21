05ad7092d9898ca8fd33c57e819479009c154de9
Anna <adurak93@gmail.com>
2020-08-21 17:00:19 +0200

    Create the table with all of the plants values.

--- src/components/plants/Plant.jsx
+++ src/components/plants/Plant.jsx
@@ -5,11 +5,47 @@ import './Plant.scss';
 class Plant extends React.PureComponent {
 
   render() {
-
     return (
-      <div>
-        <p>{ this.props.id } { ' '} { this.props.name }</p>
-      </div>
+      // <div>
+      //   <p>{ this.props.id } { this.props.name }</p>
+      // </div>
+
+                        // name={plant}
+                  // id={id}
+                  // key={index}
+                  // url={url}
+                  // category=
+                  // category_slug=
+                  // watering_interval=
+                  // fertilizing_interval=
+                  // required_exposure=
+                  // required_humidity=
+                  // required_temperature=
+                  // // blooming​=
+                  // difficulty=
+                  // // room​=
+                  // last_watered=
+                  // last_fertilized=
+
+      <tbody>
+        <tr>
+          <td>{this.props.id}</td>
+          <td>{this.props.name}</td>
+          {/*<td>{this.props.url}</td>*/}
+          <td>{this.props.category}</td>
+          {/*<td>{this.props.category_slug}</td>*/}
+          <td>{this.props.watering_interval}</td>
+          <td>{this.props.fertilizing_interval}</td>
+          <td>{this.props.required_exposure}</td>
+          <td>{this.props.required_humidity}</td>
+          <td>{this.props.required_temperature}</td>
+          {/*<td>{blooming​}</td>*/}
+          <td>{this.props.difficulty}</td>
+          {/*<td>{room​}</td>*/}
+          <td>{this.props.last_watered}</td>
+          <td>{this.props.last_fertilized}</td>
+        </tr>
+      </tbody>
     )
   }
 
--- src/components/plants/Plants.jsx
+++ src/components/plants/Plants.jsx
@@ -1,4 +1,4 @@
-import { Card, CardBody } from "reactstrap";
+import {Card, CardBody} from "reactstrap";
 import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
@@ -19,47 +19,82 @@ class Plants extends React.PureComponent {
 
   componentDidMount() {
     this.fetchPlants().finally(() => {
-      this.setState({ inProgress: false });
+      this.setState({inProgress: false});
     });
   }
 
   fetchPlants() {
     const requestUrl = "http://gentle-tor-07382.herokuapp.com/plants/";
-    this.setState({ inProgress: true });
+    this.setState({inProgress: true});
     return this.props.delayFetch(PLANTS_FETCH_DELAY, (resolve, reject) => {
       axios
         .get(requestUrl)
         .then((response) => {
           const data = response.data;
-          const plants = data.map((item) => {
-            const { id, name } = item;
-            return { id, name };
-          });
+          // const plants = data.map((item) => (item.name));
+          const plants = data.map(item => item);
           const successPlants = true;
-          this.setState({ plants, successPlants });
+          this.setState({plants, successPlants});
           resolve();
         })
         .catch((error) => {
-          this.setState({ successPlants: false });
+          this.setState({successPlants: false});
           reject();
         });
     });
   }
 
+
   render() {
-    const { plants, successPlants, inProgress } = this.state;
+    const {plants, successPlants, inProgress} = this.state;
 
     return (
       <Card className="mb-4">
         <CardBody>
-          <InProgress inProgress={inProgress} />
+          <InProgress inProgress={inProgress}/>
           {successPlants === false && <p>Nie udało się pobrać Kwiatow</p>}
           {successPlants && (
-            <div className="plants">
+            <table className="plants">
+              <thead>
+              <tr>
+                <th>id</th>
+                <th>name</th>
+                {/*<th>url</th>*/}
+                <th>category</th>
+                {/*<th>category_slug</th>*/}
+                <th>watering_interval</th>
+                <th>fertilizing_interval</th>
+                <th>required_exposure</th>
+                <th>required_humidity</th>
+                <th>required_temperature</th>
+                {/*<th>blooming​</th>*/}
+                <th>difficulty</th>
+                {/*<th>room​</th>*/}
+                <th>last_watered</th>
+                <th>last_fertilized</th>
+              </tr>
+              </thead>
               {plants.map((plant, index, arr) => (
-                <Plant name={plant} key={index} />
+                <Plant
+                  name={plant.name}
+                  id={plant.id}
+                  key={index}
+                  url={plant.url}
+                  category={plant.category}
+                  // category_slug={plant.category_slug}
+                  watering_interval={plant.watering_interval}
+                  fertilizing_interval={plant.fertilizing_interval}
+                  required_exposure={plant.required_exposure}
+                  required_humidity={plant.required_humidity}
+                  required_temperature={plant.required_temperature}
+                  // blooming​=
+                  difficulty={plant.difficulty}
+                  // room​=
+                  last_watered={plant.last_watered}
+                  last_fertilized={plant.last_fertilized}
+                />
               ))}
-            </div>
+            </table>
           )}
         </CardBody>
       </Card>
